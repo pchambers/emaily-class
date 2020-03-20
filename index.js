78547b512677e08e582d71c92295eaec625e6e66
pchambers <peter.chambers1@gmail.com>
2020-03-20 00:33:52 -0400

    init client commit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
@@ -11,6 +12,8 @@ mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+//adding middleware
+app.use(bodyParser.json());
 app.use(
   cookieSession({
     maxAge: 30*24*60*60*1000,
@@ -22,6 +25,19 @@ app.use(passport.session());
 
 //require routes with the app that was created. app gets new app handlers
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
+
+if (process.env.NODE_ENV === 'production'){
+  // Express will server production assets like main.js or main.css files
+  app.use(express.static('client/build'));
+
+  //Express to serve index.html if it doesn't recognize route.
+  const path = require('path');
+  //checkout circle ci at some point for continuous integration. 
+  app.get('*', (req,res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
---
+++ routes/billingRoutes.js
@@ -0,0 +1,19 @@
+const keys = require("../config/keys");
+const stripe = require("stripe")(keys.stripeSecret);
+const requireLogin = require("../middlewares/requireLogin");
+
+module.exports = app => {
+  app.post("/api/stripe", requireLogin, async (req, res) => {
+    const charge = stripe.charges.create({
+      amount: 5 * 100,
+      currency: "usd",
+      description: "$5 for 5 credits",
+      source: req.body.id
+    });
+
+    req.user.credits += 5;
+    const user = await req.user.save();
+
+    res.send(user);
+  });
+};
